e933bfab43d8e43a33bfe1005d2bc5e6baa1331a
freedom <furuihe@gmail.com>
2018-05-28 11:14:17 +0800

    add views dir

--- admin/src/router.ts
+++ admin/src/router.ts
@@ -2,27 +2,27 @@ import Vue from 'vue'
 import VueRouter, { Location, Route, RouteConfig } from 'vue-router'
 import { makeHot, reload } from './util/hot-reload'
 
-const homeComponent = () => import('./components/home').then(({ HomeComponent }) => HomeComponent)
-const aboutComponent = () => import('./components/about').then(({ AboutComponent }) => AboutComponent)
-const listComponent = () => import('./components/list').then(({ ListComponent }) => ListComponent)
-// const homeComponent = () => import(/* webpackChunkName: 'home' */'./components/home').then(({ HomeComponent }) => HomeComponent)
-// const aboutComponent = () => import(/* webpackChunkName: 'about' */'./components/about').then(({ AboutComponent }) => AboutComponent)
-// const listComponent = () => import(/* webpackChunkName: 'list' */'./components/list').then(({ ListComponent }) => ListComponent)
+const homeComponent = () => import('./views/home').then(({ HomeComponent }) => HomeComponent)
+const aboutComponent = () => import('./views/about').then(({ AboutComponent }) => AboutComponent)
+const listComponent = () => import('./views/list').then(({ ListComponent }) => ListComponent)
+// const homeComponent = () => import(/* webpackChunkName: 'home' */'./views/home').then(({ HomeComponent }) => HomeComponent)
+// const aboutComponent = () => import(/* webpackChunkName: 'about' */'./views/about').then(({ AboutComponent }) => AboutComponent)
+// const listComponent = () => import(/* webpackChunkName: 'list' */'./views/list').then(({ ListComponent }) => ListComponent)
 if (process.env.ENV === 'development' && module.hot) {
-  const homeModuleId = './components/home'
-  const aboutModuleId = './components/about'
-  const listModuleId = './components/list'
+  const homeModuleId = './views/home'
+  const aboutModuleId = './views/about'
+  const listModuleId = './views/list'
 
   // first arguments for `module.hot.accept` and `require` methods have to be static strings
   // see https://github.com/webpack/webpack/issues/5668
   makeHot(homeModuleId, homeComponent,
-    module.hot.accept('./components/home', () => reload(homeModuleId, (require('./components/home') as any).HomeComponent)))
+    module.hot.accept('./views/home', () => reload(homeModuleId, (require('./views/home') as any).HomeComponent)))
 
   makeHot(aboutModuleId, aboutComponent,
-    module.hot.accept('./components/about', () => reload(aboutModuleId, (require('./components/about') as any).AboutComponent)))
+    module.hot.accept('./views/about', () => reload(aboutModuleId, (require('./views/about') as any).AboutComponent)))
 
   makeHot(listModuleId, listComponent,
-    module.hot.accept('./components/list', () => reload(listModuleId, (require('./components/list') as any).ListComponent)))
+    module.hot.accept('./views/list', () => reload(listModuleId, (require('./views/list') as any).ListComponent)))
 }
 
 Vue.use(VueRouter)
